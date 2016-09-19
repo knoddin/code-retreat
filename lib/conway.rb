924779f10691cbbfaec379f9205bbb0da2abc93a
knoddin <knoddin@gmail.com>
2016-09-19 13:21:22 -0400

    started structure for conway

---
+++ lib/conway.rb
@@ -0,0 +1,14 @@
+class World(width, length, cells)
+
+  @width = width
+  @length = length
+  @cells = cells
+
+  //cell -- alive or dead? position.
+
+  def initialize(cells, lat, long)
+    @cells
+
+
+
+end
