f3d5576d710ee0d659377bd9a9dd6fcad57ef643
knoddin <knoddin@gmail.com>
2016-09-19 13:37:28 -0400

    Attempt to define board and cell constraints and attributes

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,18 +1,15 @@
-class World(width, length, cells)
+class World
+  length
+  cells = [[]]
 
 attr_accessor :lat, :long, :life
 
-  @width = width
-  @length = length
-  @cells = [[]]
-
-  //cell -- alive or dead? position.
+  # cell -- alive or dead? position.
 
   def initialize(lat, long, life)
     @lat = lat
     @long = long
     @life = life
+  end
 
-
-
-end
+  lat =
